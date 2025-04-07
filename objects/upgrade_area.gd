81413895bd4c9d46dc590241835e726168deb88e
lilkaz <39888536+lilkaz@users.noreply.github.com>
2025-04-07 17:09:52 -0400

    fixes and upgrade panel

---
+++ objects/upgrade_area.gd
@@ -0,0 +1,33 @@
+extends Control
+
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
+@onready var area: Area2D = $Area2D
+
+func _ready():
+	area.body_entered.connect(_on_area_2d_body_entered)
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		$".".visible = true
+		print("player entered")
+
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		$".".visible = false
+
+
+func _on_add_health_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		print("health pressed")
+
+
+func _on_add_shield_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		print("shield pressed")
+
+
+func _on_add_speed_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		print("speed pressed")
