73e3ccd8100f8ee847636cf5e2b0292d62bec002
lilkaz <39888536+lilkaz@users.noreply.github.com>
2025-04-07 16:04:39 -0400

    final boss updated

--- objects/boss.gd
+++ objects/boss.gd
@@ -1,36 +1,64 @@
 extends Node2D
 
-var max_health: int = 1000
-var health: int = max_health
+@export var laser_scene: PackedScene
+@export var shooting_speed: float = 600
+@export var player_path: NodePath
+var player: Node2D
 
-@export var player: Node2D
-@export var laser_scene: PackedScene  # Drag Laser.tscn here
-@export var fire_radius: float = 400
-@export var fire_cooldown: float = 1.5
+@export var detection_radius: float = 200
+@export var fire_cooldown: float = 1.0
 
-var time_since_last_shot: float = 0
+var can_shoot: bool = true
+
+var player_was_in_radius = false
 
 func _process(delta):
 	if player:
-		look_at(player.global_position)
-		$Label.text = str(health)
+		var direction_to_player = player.global_position - global_position
+		var distance_to_player = direction_to_player.length()
 
-		var distance_to_player = global_position.distance_to(player.global_position)
-		time_since_last_shot += delta
+		$AnimatedSprite2D.flip_h = direction_to_player.x < 0
 
-		if distance_to_player <= fire_radius and time_since_last_shot >= fire_cooldown:
-			fire_laser()
-			time_since_last_shot = 0
+		if distance_to_player <= detection_radius:
+			
+			if not player_was_in_radius:
+				$RockCrushing.play()
+				player_was_in_radius = true
 
-func fire_laser():
+			if can_shoot:
+				$AnimatedSprite2D.play("changing")
+				shoot_laser_to_player(direction_to_player)
+
+		else:
+			player_was_in_radius = false
+			$AnimatedSprite2D.play("idle")
+
+func _ready():
+	player = get_node(player_path)
+	$RayCast2D/ReloadTimer.timeout.connect(_on_reload_timer_timeout)
+	$RayCast2D/ReloadTimer.start()
+
+
+func _on_reload_timer_timeout():
+	can_shoot = true
+
+
+func shoot_laser_to_player(direction_to_player: Vector2):
+	
+	var distance_to_player = direction_to_player.length()
+	if distance_to_player <= detection_radius and can_shoot:
+		
+		shoot_laser(direction_to_player.normalized())
+		can_shoot = false
+		
+		
+		$RayCast2D/ReloadTimer.start()
+
+func shoot_laser(direction: Vector2):
 	var laser = laser_scene.instantiate()
-	var direction = (player.global_position - global_position).normalized()
-
-	# spawn a little bit in front of the boss
-	laser.global_position = global_position + direction * 20
-
-	# pass direction and rotate it
+	get_parent().add_child(laser)
+	laser.global_position = global_position
 	laser.direction = direction
-	laser.rotation = direction.angle()
-
-	get_tree().current_scene.add_child(laser)
+	laser.speed = shooting_speed
+	
+	$LaserSound.play()
