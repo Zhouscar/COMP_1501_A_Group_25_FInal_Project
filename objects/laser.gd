4829efc988b365cad6311b3eab06d33c62bc64b4
lilkaz <39888536+lilkaz@users.noreply.github.com>
2025-04-07 17:49:52 -0400

    laser damage

--- objects/laser.gd
+++ objects/laser.gd
@@ -6,3 +6,14 @@ var direction: Vector2 = Vector2.ZERO
 func _process(delta):
 	position += direction * speed * delta
 	
+func _ready():
+	$Area2D.body_entered.connect(_on_area_2d_body_entered)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_node("Health"):
+		var health = body.get_node("Health") as Health
+		health.damage(10)
+		print(health.current_health)
+		print("got damage")
+		queue_free()
