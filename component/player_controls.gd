131e9502c178802cf714e533daf3f42d0916ea4e
Nuo0o <semihdeli.official@gmail.com>
2025-04-07 23:26:58 -0400

    add health speed damage label

--- component/player_controls.gd
+++ component/player_controls.gd
@@ -3,11 +3,8 @@ class_name PlayerControls extends Node
 @export var body: CharacterBody2D;
 @export var collision: CollisionObject2D;
 
+var attack_in_progress = false
 
-
-
-
-	
 func _enter_tree() -> void:
 	assert(owner is Entity);
 	owner.set_meta(&"PlayerControls", self);
@@ -25,7 +22,7 @@ func _process_movement():
 	
 	if Input.is_key_pressed(KEY_SPACE):
 		movement.dash();
-		
+
 func _process_defense():
 	if (!(owner as Entity).has_component("Defense")):
 		return;
@@ -36,36 +33,49 @@ func _process_defense():
 	else:
 		defense.stop_defend();
 
-
-var attack_in_progress = false
-
 func _process_attack():
 	if !(owner as Entity).has_component("PlayerAttack"):
-		return
+		return;
 	
-	var attack = (owner as Entity).get_component("PlayerAttack") as PlayerAttack
-	
+	var attack = (owner as Entity).get_component("PlayerAttack") as PlayerAttack;
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and not attack_in_progress:
-		attack.attack()
+		attack.attack();
 		print("attacked") 
 		$"../AnimatedSprite2D".play("attack")
 		print($"../AnimatedSprite2D")
-
-		attack_in_progress = true
-	
+		attack_in_progress = true;
 	
 	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		attack_in_progress = false
-
-		
+		attack_in_progress = false;
 
 func _physics_process(delta: float) -> void:
 	_process_movement();
 	_process_defense();
 	_process_attack();
-		
 
+func _process(delta: float) -> void:
+	_update_player_stats_ui();
+
+func _update_player_stats_ui():
+	var stats_label = get_tree().get_current_scene().find_child("PlayerStatsLabel", true, false)
+	if stats_label:
+		var current_health = 0
+		var speed = 0
+		var damage = 0
+
+		if owner.has_component("Health"):
+			current_health = owner.get_component("Health").current_health
+
+		if owner.has_component("Movement"):
+			speed = owner.get_component("Movement").speed
+
+		if owner.has_component("PlayerAttack"):
+			var weapon = owner.get_component("PlayerAttack").weapon
+			if weapon:
+				damage = weapon.damage
+
+		stats_label.text = "Health: %s  |  Speed: %s  |  Damage: %s" % [current_health, speed, damage]
 
 func getInputDirection() -> Vector2:
 	var direction = Vector2.ZERO;
