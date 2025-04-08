7c4aeb7136737c132d16bb54ba707467c8c2cd58
lilkaz <39888536+lilkaz@users.noreply.github.com>
2025-04-07 21:25:18 -0400

    upgrade health fixed

--- objects/upgrade_area.gd
+++ objects/upgrade_area.gd
@@ -3,6 +3,9 @@ extends Control
 @onready var canvas_layer: CanvasLayer = $CanvasLayer
 @onready var area: Area2D = $Area2D
 
+
+@export var player: Node
+
 func _ready():
 	area.body_entered.connect(_on_area_2d_body_entered)
 
@@ -21,7 +24,9 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 func _on_add_health_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		print("health pressed")
-
+		if player and player.has_node("Health"):
+			var health = player.get_node("Health") as Health
+			health.heal(20)
 
 func _on_add_shield_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
