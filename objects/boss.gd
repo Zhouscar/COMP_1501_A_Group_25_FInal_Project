bbe7d62a9eee3dedcb59ffcf2a8dd0c7decdc059
Zhouscar <90882182+Zhouscar@users.noreply.github.com>
2025-04-07 21:41:28 -0400

    fix player attack

--- objects/boss.gd
+++ objects/boss.gd
@@ -1,64 +1,44 @@
 extends Node2D
 
+@onready var gun = $Gun
+@onready var label = $Label
 @export var laser_scene: PackedScene
-@export var shooting_speed: float = 600
-@export var player_path: NodePath
-var player: Node2D
 
-@export var detection_radius: float = 200
-@export var fire_cooldown: float = 1.0
-
-var can_shoot: bool = true
-
-var player_was_in_radius = false
-
-func _process(delta):
-	if player:
-		var direction_to_player = player.global_position - global_position
-		var distance_to_player = direction_to_player.length()
-
-		$AnimatedSprite2D.flip_h = direction_to_player.x < 0
-
-		if distance_to_player <= detection_radius:
-			
-			if not player_was_in_radius:
-				$RockCrushing.play()
-				player_was_in_radius = true
-
-			if can_shoot:
-				$AnimatedSprite2D.play("changing")
-				shoot_laser_to_player(direction_to_player)
-
-		else:
-			player_was_in_radius = false
-			$AnimatedSprite2D.play("idle")
+var health := 200
+var fire_rate := 1.5
+var fire_timer := 0.0
+var player: Node2D = null
 
 func _ready():
-	player = get_node(player_path)
-	$RayCast2D/ReloadTimer.timeout.connect(_on_reload_timer_timeout)
-	$RayCast2D/ReloadTimer.start()
+	player = get_tree().get_root().find_node("Player", true, false)
+	if not player:
+		print("⚠️ Player not found by boss!")
 
+func _process(delta):
+	if not player:
+		return
 
-func _on_reload_timer_timeout():
-	can_shoot = true
+	look_at(player.global_position)
+	label.text = "Boss HP: %s" % health
 
+	fire_timer += delta
+	if fire_timer >= fire_rate:
+		fire_timer = 0
+		shoot_laser()
 
-func shoot_laser_to_player(direction_to_player: Vector2):
-	
-	var distance_to_player = direction_to_player.length()
-	if distance_to_player <= detection_radius and can_shoot:
-		
-		shoot_laser(direction_to_player.normalized())
-		can_shoot = false
-		
-		
-		$RayCast2D/ReloadTimer.start()
+func shoot_laser():
+	if not laser_scene:
+		return
 
-func shoot_laser(direction: Vector2):
 	var laser = laser_scene.instantiate()
+	laser.global_position = gun.global_position
+	laser.rotation = gun.global_rotation
+	laser.direction = Vector2.RIGHT.rotated(global_rotation)
 	get_parent().add_child(laser)
-	laser.global_position = global_position
-	laser.direction = direction
-	laser.speed = shooting_speed
-	
-	$LaserSound.play()
+
+func _on_Area2D_area_entered(area):
+	if area.name == "Hitbox":
+		health -= 50
+		print("Boss hit! HP:", health)
+		if health <= 0:
+			queue_free()
